fb389e132bdf1e68112b792e6980144e1891795a
Rhefa Farizi <rhefa.farizi@gmail.com>
2022-12-05 21:09:24 +0700

    update index

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-// declare library
-require('dotenv').config(); // menyimpan configuration
+require('dotenv').config(); 
 const express = require('express');
-const cors = require('cors'); // membatasi akses
+const cors = require('cors');
 const cookieparser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 
-// buat route
 const userRouter = require('./src/route/user.routes.js');
 const recipeRouter = require('./src/route/recipe.routes.js')
 
 const app = express();
 
 try {
+  app.use(express.static('assets'))
   app.use(helmet());
   app.use(bodyParser.json());
   app.use(xss());
   app.use(cors());
   app.use(cookieparser());
+  app.use(userRouter);
+  app.use(recipeRouter);
+
+  app.listen(process.env.PORT, () => {
+    console.log('SERVICE IS RUNNING ON PORT ' + process.env.PORT);
+  })
 } catch (err) {
   console.log(err);
 }
-app.use(userRouter);
-app.use(recipeRouter);
 
-// jalankan express
-app.listen(process.env.PORT, () => {
-  console.log('SERVICE IS RUNNING ON PORT 3001');
-})

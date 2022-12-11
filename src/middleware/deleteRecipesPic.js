d2306efc85d9d395be212ca59042f651b06e40e0
Rhefa Farizi <rhefa.farizi@gmail.com>
2022-12-11 14:40:25 +0700

    fixed delete img

--- src/middleware/deleteRecipesPic.js
+++ src/middleware/deleteRecipesPic.js
@@ -6,7 +6,7 @@ const removeRecipeImg = async (req, res, next) => {
   const data = await recipeModel.recipeDetail(id);
   if (data.rows[0]) {
     const img = data.rows[0].recipeimg;
-    if(img !== "https://res.cloudinary.com/dmkviiqax/image/upload/v1670740075/null_jxiqhn.jpg"){
+    if(img.split('|&&|')[0] !== "https://res.cloudinary.com/dmkviiqax/image/upload/v1670740075/null_jxiqhn.jpg"){
       await cloudinary.uploader.destroy(img.split('|&&|')[1])
     }
     next();
